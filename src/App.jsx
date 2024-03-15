ddccc0facc071454716addb39e5657dc1ccf615d
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-03-15 11:02:32 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { RecursiveComponent } from "./RecursiveComponent";
+import { DangerButton, BigSuccessButton } from "./composition";
+
+const nestedObject = {
+  a: 1,
+  b: {
+    b1: 4,
+    b2: {
+      b23: "Hello",
+    },
+    b3: {
+      b31: {
+        message: "Hi",
+      },
+      b32: {
+        message: "Hi",
+      },
+    },
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  },
+};
+
+function App() {
+  return (
+    <>
+      <RecursiveComponent data={nestedObject} />
+      <DangerButton text="Don't do it!" />
+      <BigSuccessButton text="Yes!!!" />
+    </>
+  );
+}
+
+export default App;
